33675d7668f496963f85636f4de59c4e34ea85ca
Julian Lasso <julianlasso1133109@correo.itm.edu.co>
2024-04-11 00:08:49 -0500

    Add RegisterRole and RegisterRoleResponse classes to AccessControl.Domain.Aggregates.Dto namespace

---
+++ src/Backend/AccessControl/Domain/Aggregates/Helpers/RegisterRoleHelper.cs
@@ -0,0 +1,38 @@
+using AccessControl.Domain.Aggregates.Dto;
+using AccessControl.Domain.Entities;
+using AccessControl.Domain.Entities.Structs;
+using AccessControl.Domain.ValueObjects;
+using Shared.Domain.Aggregate.Helpers;
+
+namespace AccessControl.Domain.Aggregates.Helpers;
+
+internal abstract class RegisterRoleHelper : BaseHelper
+{
+    public static RegisterRoleResponse Execute(RegisterRole registerData)
+    {
+        var data = GetRoleStruct(registerData);
+        ValidateStructureFields(data);
+
+        var role = new RoleEntity();
+        role.Register(data.Name, data.Description);
+
+        return new RegisterRoleResponse
+        {
+            RoleId = role.RoleId.Value,
+            Name = role.Name.Value,
+            Description = role.Description.Value ?? string.Empty,
+            Disabled = role.IsDisabled.Value,
+        };
+    }
+
+    private static RoleStruct GetRoleStruct(RegisterRole registerData)
+    {
+        var name = new NameValueObject(registerData.Name);
+        var description =
+            registerData.Description != null
+                ? new DescriptionValueObject(registerData.Description)
+                : null;
+
+        return new RoleStruct { Name = name, Description = description, };
+    }
+}
