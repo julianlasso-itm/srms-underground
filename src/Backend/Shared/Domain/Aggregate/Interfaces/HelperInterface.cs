50d9625fb7f0d62785140cf909014b55f6bd2148
Julian Andres Lasso Figueroa <julianlasso1133109@correo.itm.edu.co>
2024-04-14 10:10:57 -0500

    Add Execute method to HelperInterface.cs

--- src/Backend/Shared/Domain/Aggregate/Interfaces/HelperInterface.cs
+++ src/Backend/Shared/Domain/Aggregate/Interfaces/HelperInterface.cs
@@ -2,5 +2,6 @@ namespace Shared.Domain.Aggregate.Interfaces;
 
 public interface IHelper<TRequest,TResponse>
 {
+    public abstract static void Execute();
     public abstract static TResponse Execute(TRequest data);
 }
