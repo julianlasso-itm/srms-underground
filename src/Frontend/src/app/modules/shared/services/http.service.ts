1b1af55b1e97ca0a4a0758b3725ef6d7bcedea6d
Julian Andres Lasso Figueroa <julianlasso1133109@correo.itm.edu.co>
2024-04-17 10:56:34 -0500

    Add HttpService class for handling HTTP requests

---
+++ src/Frontend/src/app/modules/shared/services/http.service.ts
@@ -0,0 +1,44 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HttpService {
+  constructor(private readonly httpClient: HttpClient) {}
+
+  public get<TResponse>(
+    url: string,
+    params?: HttpParams,
+    headers?: HttpHeaders
+  ): Observable<TResponse> {
+    return this.httpClient.get<TResponse>(url, { headers, params });
+  }
+
+  public post<TRequest, TResponse>(
+    url: string,
+    body: TRequest,
+    params?: HttpParams,
+    headers?: HttpHeaders
+  ): Observable<TResponse> {
+    return this.httpClient.post<TResponse>(url, body, { headers, params });
+  }
+
+  public put<TRequest, TResponse>(
+    url: string,
+    body: TRequest,
+    params?: HttpParams,
+    headers?: HttpHeaders
+  ): Observable<TResponse> {
+    return this.httpClient.put<TResponse>(url, body, { headers, params });
+  }
+
+  public delete<TResponse>(
+    url: string,
+    params?: HttpParams,
+    headers?: HttpHeaders
+  ): Observable<TResponse> {
+    return this.httpClient.delete<TResponse>(url, { headers, params });
+  }
+}
