cb740b645f48389d62289e5ebc9f436d3207a948
Julian Andres Lasso Figueroa <julianlasso1133109@correo.itm.edu.co>
2024-04-17 16:15:07 -0500

    Update CORS policy and add routing in Program.cs

--- src/Backend/ApiGateway/Infrastructure/Program.cs
+++ src/Backend/ApiGateway/Infrastructure/Program.cs
@@ -11,8 +11,7 @@ if (builder.Environment.IsDevelopment())
     AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
     var httpClientHandler = new HttpClientHandler
     {
-        ServerCertificateCustomValidationCallback = (message, cert, chain, errors) =>
-        true
+        ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true
     };
 
     builder.Services.AddSingleton(provider => new AccessControlService(httpClientHandler));
@@ -21,6 +20,18 @@ else
 {
     builder.Services.AddSingleton<AccessControlService>();
 }
+
+// Define CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(
+        "AllowAll",
+        builder =>
+        {
+            builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+        }
+    );
+});
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -35,7 +46,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("AllowAll");
+
 app.UseHttpsRedirection();
+
+app.UseRouting();
 
 app.UseAuthorization();
 
