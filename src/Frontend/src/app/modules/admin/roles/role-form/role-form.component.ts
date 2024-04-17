f7b91edf42db12bd6f8d95937a27fdb5090cc495
Julian Andres Lasso Figueroa <julianlasso1133109@correo.itm.edu.co>
2024-04-17 10:56:53 -0500

    Refactor role-form.component.html and role-form.component.ts to use reactive forms in Angular

--- src/Frontend/src/app/modules/admin/roles/role-form/role-form.component.ts
+++ src/Frontend/src/app/modules/admin/roles/role-form/role-form.component.ts
@@ -1,18 +1,60 @@
-import { Component, Input, Signal, signal } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  Signal,
+  computed,
+  signal,
+  EventEmitter,
+  OnInit,
+} from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'srms-role-form',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './role-form.component.html',
   styleUrl: './role-form.component.scss',
 })
-export class RoleFormComponent {
+export class RoleFormComponent implements OnInit {
   @Input() id: Signal<string | null>;
+  @Output('frmRole') form: EventEmitter<Signal<FormGroup>>;
+  public frmRole: Signal<FormGroup>;
+  private regexp =
+    '^[0-9A-Za-z]{8}-[0-9A-Za-z]{4}-4[0-9A-Za-z]{3}-[89ABab][0-9A-Za-z]{3}-[0-9A-Za-z]{12}$';
 
   constructor() {
     this.id = signal(null);
+    this.frmRole = signal(
+      new FormGroup({
+        name: new FormControl('', [
+          Validators.required,
+          Validators.maxLength(50),
+        ]),
+        description: new FormControl('', [Validators.maxLength(1024)]),
+      })
+    );
+    this.form = new EventEmitter<Signal<FormGroup>>();
+  }
+
+  ngOnInit(): void {
+    if (this.id() !== null) {
+      this.frmRole().setControl(
+        'id',
+        new FormControl(this.id(), [
+          Validators.required,
+          Validators.pattern(this.regexp),
+        ])
+      );
+    }
+    this.form.emit(computed(() => this.frmRole()));
   }
 }
