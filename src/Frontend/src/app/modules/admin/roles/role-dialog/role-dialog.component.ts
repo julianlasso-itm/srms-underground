0a33520ba50ee9f8c3274af0588e49ee8ac7173b
Julian Andres Lasso Figueroa <julianlasso1133109@correo.itm.edu.co>
2024-04-17 10:57:15 -0500

    Refactor role-dialog.component.html and role-dialog.component.ts to add form submission functionality

--- src/Frontend/src/app/modules/admin/roles/role-dialog/role-dialog.component.ts
+++ src/Frontend/src/app/modules/admin/roles/role-dialog/role-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, signal, type Signal } from '@angular/core';
+import { Component, Inject, signal, Signal, type Output } from '@angular/core';
 import {
   MAT_DIALOG_DATA,
   MatDialogModule,
@@ -7,24 +7,39 @@ import {
 import { DialogType, FormType } from './dialog.type';
 import { RoleFormComponent } from '../role-form/role-form.component';
 import { MatButtonModule } from '@angular/material/button';
+import { HttpService } from '../../../shared/services/http.service';
+import { HttpClientModule } from '@angular/common/http';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'srms-role-dialog',
   standalone: true,
-  imports: [MatButtonModule, MatDialogModule, RoleFormComponent],
+  imports: [
+    MatButtonModule,
+    MatDialogModule,
+    RoleFormComponent,
+    HttpClientModule,
+  ],
+  providers: [HttpService],
   templateUrl: './role-dialog.component.html',
   styleUrl: './role-dialog.component.scss',
 })
 export class RoleDialogComponent {
   signal = signal;
   formType = signal(FormType);
+  formRole!: Signal<FormGroup>;
 
   constructor(
     public readonly dialogRef: MatDialogRef<RoleDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: Signal<DialogType>
+    @Inject(MAT_DIALOG_DATA) public data: Signal<DialogType>,
+    public readonly httpService: HttpService
   ) {}
 
   onCancelClick(): void {
     this.dialogRef.close();
+  }
+
+  roleForm(form: Signal<FormGroup>): void {
+    this.formRole = form;
   }
 }
