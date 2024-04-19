29db4f878ebd8cb33c558f657d5cb90d7a6d4676
Julian Andres Lasso Figueroa <julianlasso1133109@correo.itm.edu.co>
2024-04-18 19:43:10 -0500

    Add SharedModule to handle shared functionality in Angular

---
+++ src/Frontend/src/app/modules/shared/shared.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpService } from './services/http.service';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, HttpClientModule],
+  providers: [HttpService],
+  exports: [],
+})
+export class SharedModule {}
