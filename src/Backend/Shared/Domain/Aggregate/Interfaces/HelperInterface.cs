91ddfab5fec2c0c9437a64217772480266a07af9
Julian Andres Lasso Figueroa <julianlasso1133109@correo.itm.edu.co>
2024-04-19 22:21:28 -0500

    Refactor HelperInterface.cs to remove abstract static method

--- src/Backend/Shared/Domain/Aggregate/Interfaces/HelperInterface.cs
+++ src/Backend/Shared/Domain/Aggregate/Interfaces/HelperInterface.cs
@@ -2,6 +2,5 @@ namespace Shared.Domain.Aggregate.Interfaces;
 
 public interface IHelper<TRequest,TResponse>
 {
-    public abstract static void Execute();
     public abstract static TResponse Execute(TRequest data);
 }
