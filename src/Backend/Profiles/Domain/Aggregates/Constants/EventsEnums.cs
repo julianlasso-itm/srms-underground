52bc01846bdd719d2abb72c663ed591cb70306db
Julian Andres Lasso Figueroa <julianlasso1133109@correo.itm.edu.co>
2024-04-22 20:11:07 -0500

    Add new event constant for country registration

--- src/Backend/Profiles/Domain/Aggregates/Constants/EventsEnums.cs
+++ src/Backend/Profiles/Domain/Aggregates/Constants/EventsEnums.cs
@@ -3,4 +3,5 @@ namespace Profiles.Domain.Aggregates.Constants;
 public abstract class EventsConst
 {
     public const string Prefix = "Profiles";
+    public const string EventCountryRegistered = "CountryRegistered";
 }
