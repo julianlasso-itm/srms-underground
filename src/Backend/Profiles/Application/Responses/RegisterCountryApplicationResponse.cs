74e2cb1a173a7fd8b058c8ac67fac580948b5bf2
Julian Andres Lasso Figueroa <julianlasso1133109@correo.itm.edu.co>
2024-04-22 20:12:12 -0500

    Add country-related application response classes in the Profiles domain

---
+++ src/Backend/Profiles/Application/Responses/RegisterCountryApplicationResponse.cs
@@ -0,0 +1,8 @@
+namespace Profiles.Application.Responses;
+
+public sealed class RegisterCountryApplicationResponse
+{
+    public required string CountryId { get; init; }
+    public required string Name { get; init; }
+    public required bool Disabled { get; init; }
+}
