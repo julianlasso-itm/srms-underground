f415c6d73fe8a27f5a8e1ee209e4f2257aa500af
Julian Andres Lasso Figueroa <julianlasso1133109@correo.itm.edu.co>
2024-04-24 20:20:06 -0500

    Refactor and add update request and response classes for city, country, and province profiles

--- src/Backend/Shared/Infrastructure/ProtocolBuffers/Profiles/Requests/UpdateCityRequest.cs
+++ src/Backend/Shared/Infrastructure/ProtocolBuffers/Profiles/Requests/UpdateCityRequest.cs
@@ -3,14 +3,17 @@ using System.Runtime.Serialization;
 namespace Shared.Infrastructure.ProtocolBuffers.Profiles.Requests;
 
 [DataContract]
-public class UpdateCountryRequest
+public class UpdateCityRequest
 {
     [DataMember(Order = 1, IsRequired = false)]
-    public string? CountryId { get; set; }
+    public string? CityId { get; set; }
 
     [DataMember(Order = 2, IsRequired = false)]
-    public string? Name { get; set; }
+    public string? ProvinceId { get; set; }
 
     [DataMember(Order = 3, IsRequired = false)]
+    public string? Name { get; set; }
+
+    [DataMember(Order = 4, IsRequired = false)]
     public bool? Disable { get; set; }
 }
--- src/Backend/Shared/Infrastructure/ProtocolBuffers/Profiles/Responses/UpdateCityResponse.cs
+++ src/Backend/Shared/Infrastructure/ProtocolBuffers/Profiles/Responses/UpdateCityResponse.cs
@@ -3,14 +3,17 @@ using System.Runtime.Serialization;
 namespace Shared.Infrastructure.ProtocolBuffers.Profiles.Responses;
 
 [DataContract]
-public class UpdateCountryResponse
+public class UpdateCityResponse
 {
     [DataMember(Order = 1)]
-    public required string CountryId { get; set; }
+    public required string CityId { get; set; }
 
     [DataMember(Order = 2)]
-    public string? Name { get; set; }
+    public string? ProvinceId { get; set; }
 
     [DataMember(Order = 3)]
+    public string? Name { get; set; }
+
+    [DataMember(Order = 4)]
     public bool? Disabled { get; set; }
 }
