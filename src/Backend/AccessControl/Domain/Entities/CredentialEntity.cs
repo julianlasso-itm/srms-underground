ab27f71b9a8af8402952b24c8e8e6f8bef2d6793
Julian Lasso <julianlasso1133109@correo.itm.edu.co>
2024-05-16 20:59:46 -0500

    refactor: Update CredentialEntity to use value objects for properties

--- src/Backend/AccessControl/Domain/Entities/CredentialEntity.cs
+++ src/Backend/AccessControl/Domain/Entities/CredentialEntity.cs
@@ -1,83 +1,84 @@
 using AccessControl.Domain.Entities.Structs;
 using AccessControl.Domain.ValueObjects;
 
-namespace AccessControl.Domain.Entities;
-
-internal sealed class CredentialEntity
+namespace AccessControl.Domain.Entities
 {
-  public CredentialIdValueObject CredentialId { get; private set; }
-  public FullNameValueObject Name { get; private set; }
-  public EmailValueObject Email { get; private set; }
-  public PasswordValueObject Password { get; private set; }
-  public AvatarValueObject Avatar { get; private set; }
-  public DisabledValueObject Disabled { get; private set; }
-  public ICollection<RoleEntity> Roles { get; private set; } = new List<RoleEntity>();
-
-  public CredentialEntity() { }
-
-  public CredentialEntity(CredentialStruct data)
+  internal sealed class CredentialEntity
   {
-    CredentialId = data.CredentialId;
-    Name = data.Name;
-    Email = data.Email;
-    Password = data.Password;
-    Avatar = data.Avatar;
-    Disabled = data.Disabled;
-  }
+    public CredentialIdValueObject CredentialId { get; private set; }
+    public FullNameValueObject Name { get; private set; }
+    public EmailValueObject Email { get; private set; }
+    public PasswordValueObject Password { get; private set; }
+    public AvatarValueObject Avatar { get; private set; }
+    public DisabledValueObject Disabled { get; private set; }
+    public ICollection<RoleEntity> Roles { get; private set; } = new List<RoleEntity>();
 
-  public void Register(
-    FullNameValueObject name,
-    EmailValueObject email,
-    PasswordValueObject password,
-    AvatarValueObject avatar,
-    DisabledValueObject? disabled
-  )
-  {
-    CredentialId = new CredentialIdValueObject(Guid.NewGuid().ToString());
-    Name = name;
-    Email = email;
-    Password = password;
-    Avatar = avatar;
-    Disabled = disabled ?? new DisabledValueObject(false);
-  }
+    public CredentialEntity() { }
 
-  public void UpdatePassword(PasswordValueObject password)
-  {
-    Password = password;
-  }
+    public CredentialEntity(CredentialStruct data)
+    {
+      CredentialId = data.CredentialId;
+      Name = data.Name;
+      Email = data.Email;
+      Password = data.Password;
+      Avatar = data.Avatar;
+      Disabled = data.Disabled;
+    }
 
-  public void Enable()
-  {
-    Disabled = new DisabledValueObject(false);
-  }
+    public void Register(
+      FullNameValueObject name,
+      EmailValueObject email,
+      PasswordValueObject password,
+      AvatarValueObject avatar,
+      DisabledValueObject? disabled
+    )
+    {
+      CredentialId = new CredentialIdValueObject(Guid.NewGuid().ToString());
+      Name = name;
+      Email = email;
+      Password = password;
+      Avatar = avatar;
+      Disabled = disabled ?? new DisabledValueObject(false);
+    }
 
-  public void Disable()
-  {
-    Disabled = new DisabledValueObject(true);
-  }
+    public void UpdatePassword(PasswordValueObject password)
+    {
+      Password = password;
+    }
 
-  public void UpdateAvatar(AvatarValueObject avatar)
-  {
-    Avatar = avatar;
-  }
+    public void Enable()
+    {
+      Disabled = new DisabledValueObject(false);
+    }
 
-  public void UpdateName(FullNameValueObject name)
-  {
-    Name = name;
-  }
+    public void Disable()
+    {
+      Disabled = new DisabledValueObject(true);
+    }
 
-  public void AddRole(RoleEntity role)
-  {
-    Roles.Add(role);
-  }
+    public void UpdateAvatar(AvatarValueObject avatar)
+    {
+      Avatar = avatar;
+    }
 
-  public void RemoveRole(RoleEntity role)
-  {
-    Roles.Remove(role);
-  }
+    public void UpdateName(FullNameValueObject name)
+    {
+      Name = name;
+    }
 
-  public void ClearRoles()
-  {
-    Roles.Clear();
+    public void AddRole(RoleEntity role)
+    {
+      Roles.Add(role);
+    }
+
+    public void RemoveRole(RoleEntity role)
+    {
+      Roles.Remove(role);
+    }
+
+    public void ClearRoles()
+    {
+      Roles.Clear();
+    }
   }
 }
