d0c78665c047d0fbde5a1cdd1623b644052b30a5
Julian Lasso <julianlasso1133109@correo.itm.edu.co>
2024-05-18 23:32:44 -0500

    refactor: Remove BaseGetRequest class and update related requests

--- src/Backend/Shared/Infrastructure/ProtocolBuffers/Profiles/Requests/GetProvincesProfilesRequest.cs
+++ src/Backend/Shared/Infrastructure/ProtocolBuffers/Profiles/Requests/GetProvincesProfilesRequest.cs
@@ -1,8 +1,26 @@
 using System.Runtime.Serialization;
-using Infrastructure.ProtocolBuffers.Base;
 
 namespace Shared.Infrastructure.ProtocolBuffers.Profiles.Requests
 {
   [DataContract]
-  public class GetProvincesProfilesRequest : BaseGetRequest { }
+  public class GetProvincesProfilesRequest
+  {
+    [DataMember(Order = 1)]
+    public int Page { get; set; }
+
+    [DataMember(Order = 2)]
+    public int Limit { get; set; }
+
+    [DataMember(Order = 3, IsRequired = false)]
+    public string? Filter { get; set; }
+
+    [DataMember(Order = 4, IsRequired = false)]
+    public string? FilterBy { get; set; }
+
+    [DataMember(Order = 5, IsRequired = false)]
+    public string? Sort { get; set; }
+
+    [DataMember(Order = 6, IsRequired = false)]
+    public string? Order { get; set; } = "asc";
+  }
 }
--- src/Backend/Shared/Infrastructure/Services/CacheService.cs
+++ src/Backend/Shared/Infrastructure/Services/CacheService.cs
@@ -1,4 +1,3 @@
-using System.Text;
 using Shared.Application.Interfaces;
 using StackExchange.Redis;
 
