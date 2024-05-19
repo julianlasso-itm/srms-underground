cfa56ecb65ea063e17bcc001246249a04bb0026b
SebastianCamargo-ITM <juancamargo1135122@correo.itm.edu.co>
2024-05-19 04:11:09 -0500

    Se valida rol

--- src/Frontend/src/app/templates/main/app.component.ts
+++ src/Frontend/src/app/templates/main/app.component.ts
@@ -14,14 +14,12 @@ import { Constant } from '../../modules/shared/constants/constants';
 })
 export class AppComponent implements OnInit {
   isAuth: boolean;
-  isAdmin: boolean;
   private authObservable!: Subscription;
   public profile: ProfileModel = new ProfileModel();
 
   constructor(private authService: AuthService, private route: Router) {
     this.isAuth = this.authService.isAuthenticated();
     this.profile = this.authService.getTokenData();
-    this.isAdmin = this.validateAdmin();
   }
   
   ngOnInit() {
@@ -30,7 +28,7 @@ export class AppComponent implements OnInit {
     });
   }
 
-  validateAdmin(): boolean {
+  isAdmin(): boolean {
     return this.profile.Roles.includes(Constant.ADMIN_ROLE);
   }
 }
