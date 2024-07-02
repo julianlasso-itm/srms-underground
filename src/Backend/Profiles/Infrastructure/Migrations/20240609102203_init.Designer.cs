14dec6e83f0758ea13a7f9971226332489628103
Julian Lasso <julianlasso1133109@correo.itm.edu.co>
2024-07-02 10:21:33 -0500

    refactor: Update migration class names to use PascalCase

--- src/Backend/Profiles/Infrastructure/Migrations/20240609102203_init.Designer.cs
+++ src/Backend/Profiles/Infrastructure/Migrations/20240609102203_init.Designer.cs
@@ -13,7 +13,7 @@ namespace Profiles.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
     [Migration("20240609102203_init")]
-    partial class init
+    partial class Init
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
