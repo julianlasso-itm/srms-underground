89725bdee4f00932d41ea90d7fa4b30537c8a91f
Julian Lasso <julianlasso1133109@correo.itm.edu.co>
2024-07-17 21:18:02 -0500

    se hace el diseño del home y la información está quemada

--- src/Frontend/src/app/modules/home/home.component.ts
+++ src/Frontend/src/app/modules/home/home.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'srms-home',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, MatIconModule, MatTooltipModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon(
+      'angular',
+      sanitizer.bypassSecurityTrustResourceUrl('icons/angular.svg')
+    );
+    iconRegistry.addSvgIcon(
+      'nodejs',
+      sanitizer.bypassSecurityTrustResourceUrl('icons/node-js.svg')
+    );
+    iconRegistry.addSvgIcon(
+      'mongodb',
+      sanitizer.bypassSecurityTrustResourceUrl('icons/mongodb.svg')
+    );
+  }
+}
